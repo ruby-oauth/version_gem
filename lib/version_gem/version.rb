b9380eda48fe007b13a17a71e95a04c18d34beb2
Peter H. Boling <peter.boling@gmail.com>
2026-07-31 23:06:23 -0600

    🎨 Template bootstrap by kettle-jem v7.1.0

--- lib/version_gem/version.rb
+++ lib/version_gem/version.rb
@@ -1,8 +1,11 @@
 # frozen_string_literal: true
 
 module VersionGem
+  # Version namespace for this gem.
   module Version
+    # Current gem version.
     VERSION = "1.1.15"
   end
+  # Current gem version exposed at the traditional constant location.
   VERSION = Version::VERSION # Traditional Constant Location
 end
